401c9bfa78d2d5954411db9379d1a0a8662f5ea7
Jiyang Jiang <54608573+JiyangJiang@users.noreply.github.com>
2023-06-16 15:27:22 +1000

    Update EXAMPLE_run_ud2.m

--- wmh/EXAMPLE_run_ud2.m
+++ wmh/EXAMPLE_run_ud2.m
@@ -14,23 +14,23 @@ spm_dir = 'C:\Users\z3402744\OneDrive - UNSW\previously on onedrive\Documents\Gi
 
 addpath (fullfile (svdd_dir, 'wmh', 'ud2'));
 
-n_workers = 2;
-save_dskspc = false;
-save_more_dskspc = false;
-verbose = true;
+% n_workers = 2;
+% save_dskspc = false;
+% save_more_dskspc = false;
+% verbose = true;
 
-temp_opt = {'existing'; '65to75'}; % options = {'existing'; '65to75'}, {'exsiting'; '70to80'}, or {'creating'}
+% temp_opt = {'existing'; '65to75'}; % options = {'existing'; '65to75'}, {'exsiting'; '70to80'}, or {'creating'}
 
-lv1clstMethod = 'superpixels'; % options = 'kmeans', 'superpixels', or 'fslfast' (in development)
-k4kmeans = 6;
-k4knn    = 5;
-n4superpixel = 5000;
-probthr = 0.7;
-extSpace = 'dartel'; % options = 'dartel' or 'native'
+% lv1clstMethod = 'superpixels'; % options = 'kmeans', 'superpixels', or 'fslfast' (in development)
+% k4kmeans = 6;
+% k4knn    = 5;
+% n4superpixel = 5000;
+% probthr = 0.7;
+% extSpace = 'dartel'; % options = 'dartel' or 'native'
 
-pvmag = 12; % distance threshold from lateral ventricles to separate PVWMH and DWMH. 12 works well in most cases.
+% pvmag = 12; % distance threshold from lateral ventricles to separate PVWMH and DWMH. 12 works well in most cases.
 
-sizthr_mm3 = [10.125 30.375 50.625]; % size thresholds in mm^3 to separate punctuate, focal, medium, confluent WMH clusters.
+% sizthr_mm3 = [10.125 30.375 50.625]; % size thresholds in mm^3 to separate punctuate, focal, medium, confluent WMH clusters.
 									 % [10.125 30.375 50.625] corresponds to 3, 9 and 15 voxels in DARTEL space.
 
 % run ud2
